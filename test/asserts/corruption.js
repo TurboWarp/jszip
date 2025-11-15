34937fb59a45eafbe6ad101ec7e57e8431356f2e
Thomas Weber <muffin@muffin.ink>
2025-11-15 15:29:22 -0600

    Add callbacks to monitor zip recovery

--- test/asserts/corruption.js
+++ test/asserts/corruption.js
@@ -1,7 +1,7 @@
 "use strict";
 
 QUnit.module("corruption", function () {
-    JSZipTestUtils.testZipFile("load(string) works", "ref/no-central-directory/truncated.zip", function(assert, file) {
+    JSZipTestUtils.testZipFile("recovers zip without central directory", "ref/no-central-directory/truncated.zip", function(assert, file) {
         var done = assert.async();
         JSZip.loadAsync(file, {
             recoverCorrupted: true
@@ -12,6 +12,31 @@ QUnit.module("corruption", function () {
             .then(function(result) {
                 const parsed = JSON.parse(result);
                 assert.equal(parsed.targets[0].name, "Stage");
+                done();
+            })
+            .catch(JSZipTestUtils.assertNoError);
+    });
+
+    JSZipTestUtils.testZipFile("calls onCorruptCentralDirectory and onUnrecoverableFileEntry", "ref/no-central-directory/more-truncated.zip", function(assert, file) {
+        var done = assert.async();
+        var centralDirectoryErrors = [];
+        var fileEntryErrors = [];
+        JSZip.loadAsync(file, {
+            recoverCorrupted: true,
+            onCorruptCentralDirectory: (error) => {
+                centralDirectoryErrors.push(error.message);
+            },
+            onUnrecoverableFileEntry: (error) => {
+                fileEntryErrors.push(error.message);
+            }
+        })
+            .then(function () {
+                assert.deepEqual(centralDirectoryErrors, [
+                    "Corrupted zip: can't find end of central directory"
+                ]);
+                assert.deepEqual(fileEntryErrors, [
+                    "End of data reached (data length = 1000, asked index = 2194). Corrupted zip ?"
+                ]);
                 done();
             })
             .catch(JSZipTestUtils.assertNoError);
